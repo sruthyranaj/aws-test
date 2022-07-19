2af38e665e68a9fd6afef241bfd81aac943206e3
sruthyranaj <sruthycty@gmail.com>
2022-07-19 12:42:31 +0530

    committed api

--- startup.py
+++ startup.py
@@ -1,5 +1,22 @@
 import uvicorn
-from webapp import app
+
+from fastapi import FastAPI, APIRouter
+
+# Initialize app instance
+app = FastAPI(title="Demo",
+              description="Demo App",
+              version="V1.0.0",
+              docs_url="/docs")
+
+
+router = APIRouter(prefix="/api")
+
+
+@router.get("/test")
+def app_version():
+    return {"data": {"status": "Success"}}
+
+app.include_router(router)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=5000)
